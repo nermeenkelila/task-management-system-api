884163757b7cc13469497b1fe4a429e469a10db8
nermeenkelila <nermeen_kelila@hotmail.com>
2025-08-29 11:06:28 +0300

    Handle task policy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TaskPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->isManager();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Task $task): bool
+    {
+        return $user->isManager() || $task->assignee_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isManager();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Task $task): bool
+    {
+        return $user->isManager();
+    }
+
+    public function assign(User $user): bool
+    {
+        return $user->isManager();
+    }
+
+    public function updateStatus(User $user, Task $task): bool
+    {
+        return $user->isManager() || $task->assignee_id == $user->id;
+    }
+
+     public function delete(User $user, Task $task)
+    {
+        return $user->isManager() && $task->created_by === $user->id;
+    }
+
+    
+}
